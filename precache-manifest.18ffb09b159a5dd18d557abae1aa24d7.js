ecd427e52deff8a9e4717cf7d54482978a637342
babyazalea <usun12@hotmail.com>
2019-05-01 16:29:39 +0900

    Updates

---
+++ precache-manifest.18ffb09b159a5dd18d557abae1aa24d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79a58c9eecb6c1c1552d8fad1ebb96c",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "daaf80012bf94922f7f4",
+    "url": "/myapp/static/css/main.a52d1962.chunk.css"
+  },
+  {
+    "revision": "ad63ab08a2a8e5504836",
+    "url": "/myapp/static/js/2.7b834e7d.chunk.js"
+  },
+  {
+    "revision": "daaf80012bf94922f7f4",
+    "url": "/myapp/static/js/main.449f20bd.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
